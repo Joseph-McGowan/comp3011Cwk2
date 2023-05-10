a25c75f09212fba4c45c849c723755f68f9dc129
Joseph McGowan <sc20jdm@leeds.ac.uk>
2023-05-10 13:39:33 +0100

    added payment validation

--- Cw2/payments/pages/views.py
+++ Cw2/payments/pages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import requests 
 import json
+import re
 
 
 # Create your views here.
@@ -16,13 +17,14 @@ url = "http://127.0.0.1:8000/payments"
 @csrf_exempt
 def paymentsForm(request):
     if request.method == "GET":
-        fields = {'fields':{'cardNumber' : 'string', 'CVV': 'string', 'expiryDate' : 'date', 'name' : 'string', 'Email' : 'string'}}
+        fields = {'fields':{'cardNumber' : 'string', 'CVV': 'string', 'expiryDate' : 'string', 'name' : 'string', 'email' : 'string'}}
         return JsonResponse(fields)
 
 
 @csrf_exempt 
 def paymentsPay(request):
     if request.method == "POST":
+        print(request.POST)
         for items in request.POST.items():
             print(items)
         cardNumber = request.POST['cardNumber']
@@ -31,6 +33,24 @@ def paymentsPay(request):
         name = request.POST['name']
         email = request.POST['email']
 
+        cardNumRegEx = re.compile(r"[0-9]{16}")
+        cvvRegEx = re.compile(r"[0-9]{3}")
+        expiryDateRegEx = re.compile(r"[0-9]{2}\/[0-9]{2}")
+        nameRegex = re.compile(r"[A-Z)][a-zA-Z]*")
+        emailRegEx = re.compile(r"[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]+")
+
+        if re.fullmatch(cardNumRegEx, cardNumber):
+            return HttpResponseBadRequest('Malformed Card Number, payment failed', status = 405)
+        if re.fullmatch(cvvRegEx, cvv):
+            return HttpResponseBadRequest('Malformed CVV, payment failed', status = 405)
+        if re.fullmatch(expiryDateRegEx, expiryDate):
+            return HttpResponseBadRequest('Malformed Expiry Date, payment failed', status = 405)
+        if re.fullmatch(nameRegex, name):
+            return HttpResponseBadRequest('Invalid Name, payment failed', status = 405)
+        if re.fullmatch(emailRegEx, email):
+            return HttpResponseBadRequest('Inavlid email, payment failed', status = 405)
+
+        
         #error checking values
 
         success = 1
@@ -40,7 +60,7 @@ def paymentsPay(request):
             print ('failure')
 
 
-        return JsonResponse({'request' : 'success'})
+        return JsonResponse({'request' : email})
 
 
         return HttpResponse('tesst')
